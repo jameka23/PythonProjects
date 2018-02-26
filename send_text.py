b4ec66bef585749d08f0893dbe59d3d76b6b84ad
Jameka Echols <jameka.echols>
2018-02-25 19:47:10 -0600

    adding twilio phone number

--- send_text.py
+++ send_text.py
@@ -1,15 +1,16 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+account_sid = "ACdf7825efd19c0f9c084a2d62dcf1004b"
 # Your Auth Token from twilio.com/console
-auth_token  = "your_auth_token"
+auth_token  = "32543c73a464d04c890b663356350a25"
+
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+15558675309", 
-    from_="+15017250604",
+    to="+16189779837", 
+    from_="+16152099156",
     body="Hello from Python!")
 
 print(message.sid)
